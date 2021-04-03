e7f420494742d3d239e42a680af49bb193767513
rahman9868 <rahman9868@gmail.com>
2021-04-03 13:52:27 +0700

    Version 0.0.1 - Design Layout with Stateless Widget (color, text, font, etc)

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home()
+  ));
+
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('crxlunchs firts app'),
+        centerTitle: true,
+        backgroundColor: Colors.purple[200],
+      ),
+      body: Center(
+        child: Text(
+          'Hello Crxlunch',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+      ),
+    );
+  }
+}
+
